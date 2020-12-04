e66a2737ee05671b77c79b5460d119b61fda7929
DanielGara <yo@danielgara.com>
2020-12-04 10:58:25 -0500

    First version

---
+++ src/main/java/variamos/componentmanagement/Main.java
@@ -0,0 +1,11 @@
+package variamos.componentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
