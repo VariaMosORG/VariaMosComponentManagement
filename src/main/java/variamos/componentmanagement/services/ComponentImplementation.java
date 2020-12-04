272595e73f52ae36c20b1fed8236258abcca7572
DanielGara <yo@danielgara.com>
2020-12-04 14:27:36 -0500

    rest home service implemented

--- src/main/java/variamos/componentmanagement/services/ComponentImplementation.java
+++ src/main/java/variamos/componentmanagement/services/ComponentImplementation.java
@@ -1,14 +1,18 @@
 package variamos.componentmanagement.services;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
 public class ComponentImplementation {
+	
+	@CrossOrigin
 	@RequestMapping("/test")
 	String home() {
-		return "Hello World!";
+		return "Ok";
 	}
+	
 }
