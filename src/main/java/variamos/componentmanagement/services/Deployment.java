413e1bcbd0dbcedc9cc407ab9a92215249615c0d
Daniel Correa <yo@danielgara.com>
2022-04-15 19:20:34 -0500

    imp: deployment

---
+++ src/main/java/variamos/componentmanagement/services/Deployment.java
@@ -0,0 +1,118 @@
+package variamos.componentmanagement.services;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
+import org.eclipse.jgit.api.RemoteAddCommand;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@RestController
+@EnableAutoConfiguration
+public class Deployment {
+
+	public static String derived_folder = "component_derived/";
+	public static String deployment_main_folder = "deployment/";
+
+	@CrossOrigin
+	@RequestMapping("/deployment/test")
+	String home() {
+		return "Ok";
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/deployment/uploadAppsToGitHub", method = RequestMethod.POST, produces = "text/plain")
+	@ResponseBody
+	String uploadAppsToGitHub(@RequestBody String data_collected) {
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj = parser.parse(data_collected).getAsJsonObject();
+		JsonElement apps = rootObj.get("apps");
+		String branchName = apps.getAsString();
+
+		JsonElement p_derived = rootObj.get("p_derived");
+		derived_folder = p_derived.getAsString();
+
+		try {
+			File appDeploymentDirectory = new File(deployment_main_folder + "/" + branchName);
+			if (appDeploymentDirectory.exists()) {
+				FileUtils.forceDelete(appDeploymentDirectory);
+			}
+
+			Git git = Git.cloneRepository().setURI("https://github.com/danielgara/todosSPL.git")
+					.setDirectory(appDeploymentDirectory).setCloneAllBranches(true).call();
+
+			try {
+				boolean existingBranch = false;
+				List<Ref> call = git.branchList().setListMode(ListMode.ALL).call();
+				for (Ref ref : call) {
+					String currentBranch = ref.getName().substring(ref.getName().lastIndexOf('/') + 1);
+					if (currentBranch.equals(branchName)) {
+						existingBranch = true;
+					}
+				}
+
+				if (existingBranch) {
+					git.checkout().setName(branchName).setCreateBranch(true)
+						.setStartPoint("origin/" + branchName).call();
+				} else {
+					git.branchCreate().setForce(true).setName(branchName).call();
+					git.checkout().setName(branchName).call();
+				}
+
+				File appDeploymentDirectoryUpdated = new File(deployment_main_folder + "/" + branchName);
+
+				for (File file : appDeploymentDirectoryUpdated.listFiles()) {
+					if (file.getName().equals(".git")) {
+						// Do nothing
+					} else {
+						file.delete();
+					}
+				}
+
+				File appDirectory = new File(derived_folder + "/" + branchName);
+				FileUtils.copyDirectory(appDirectory, appDeploymentDirectory);
+
+				git.add().addFilepattern(".").call(); // stage new files
+				git.add().setUpdate(true).addFilepattern(".").call(); // stage removed files
+				git.commit().setMessage("Initial commit").call();
+
+				RemoteAddCommand remoteAddCommand = git.remoteAdd();
+				remoteAddCommand.setName("origin");
+				remoteAddCommand.setUri(new URIish("git@github.com:danielgara/todosSPL.git"));
+				remoteAddCommand.call();
+
+				CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(
+						"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", "");
+
+				git.push().setRemote("origin").setCredentialsProvider(credentialsProvider).call();
+
+				git.getRepository().close();
+				git.close();
+			} catch (Exception e) {
+				git.getRepository().close();
+				git.close();
+				return "error" + e.getMessage();
+			}
+		} catch (Exception e) {
+			return "error" + e.getMessage();
+		}
+
+		return "Success";
+	}
+}
